3f010018268c7ae79bcd8aaa05c6f2ecda2368d2
SUNDHIR SINGH <happysohil1234@gmail.com>
2026-04-27 14:06:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "https://cdn.skypack.dev/axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5000/api"
+    baseURL: "https://staymatch.onrender.com/api"
 });
 
 export default API;
